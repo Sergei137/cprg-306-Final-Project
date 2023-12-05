71c42f3efa01756d57e90b495c6f6c96723b4ea2
Sergei137 <sm1.618@protonmail.com>
2023-12-04 17:19:02 -0700

    added pages folder and moved toDoListPage into it. Currently debugging

--- app/page.js
+++ app/page.js
@@ -1,15 +1,17 @@
+// import Image from 'next/image';
+// import ToDoList from './components/ToDoList';
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
-import Image from 'next/image';
-import ToDoList from './components/ToDoList';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Link from 'next/link';
+import ToDoListPage from './pages/ToDoListPage';
 
-
- export default function Home(){
-
+export default function Home(){
   return(
     <div>
-      <ToDoList/>
+      <p>home page</p>
+      <Link href="/ToDoListPage">
+        <a>to do list</a>
+      </Link>
     </div>
   )
-
- }
+}
---
+++ app/pages/toDoListPage.js
@@ -0,0 +1,15 @@
+
+import Image from 'next/image';
+import ToDoList from './components/ToDoList';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+ export default function ToDoListPage(){
+
+  return(
+    <div>
+      <ToDoList/>
+    </div>
+  )
+
+ }
