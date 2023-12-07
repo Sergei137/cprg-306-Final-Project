4b4c3f9c9bcc7082d9acb085aef40f3bee6e1a3c
Ryan Back <ho0405@gmail.com>
2023-12-06 20:05:14 -0700

    set up for 100 characters of description

--- app/projectDetails/page.js
+++ app/projectDetails/page.js
@@ -45,35 +45,57 @@ const ProjectDetailsPage = () => {
     localStorage.setItem('projects', JSON.stringify(updatedProjects));
   };
 
+  const formatTeamMembers = (teamMembers) => {
+    if (Array.isArray(teamMembers)) {
+      // If it's an array, join the elements with a comma
+      return teamMembers.join(', ');
+    } else if (typeof teamMembers === 'string') {
+      // If it's a string, return as is
+      return teamMembers;
+    } else {
+      // If it's neither (or undefined), show 'No team members'
+      return 'No team members';
+    }
+  };
+
+  const truncateText = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return { truncated: `${text.substring(0, maxLength)}...`, isTruncated: true };
+    } else {
+      return { truncated: text, isTruncated: false };
+    }
+  };
+
   return (
     <main>
       <NavBar />
       <div className='content-container'>
         <h2>Project Details</h2>
         <CreateProject save={handleSaveProject} />
-        {projects.map((project, index) => (
+        {projects.map((project, index) => {
+          const { truncated, isTruncated } = truncateText(project.Description, 100);
+           // Truncate after 100 characters}
+          return (
           <div key={index}>
             <p><strong>Project Name:</strong> {project.Name}</p>
-            <p><strong>Description:</strong> {project.Description}</p>
+            <p className="project-description">
+                <strong>Description:</strong> {truncated}
+                {isTruncated && <button onClick={() => alert(project.Description)}>Read more</button>}
+              </p>
             <p><strong>Due Date:</strong> {project.Duedate}</p>
-            <p><strong>Team Member:</strong> {project.Teammember}</p>
+            <p><strong>Team Member:</strong> {formatTeamMembers(project.Teammember)}</p>
             <p><strong>Progress:</strong> {project.Progress}%</p>
             <progress value={project.Progress} max="100"></progress>
             <button onClick={() => handleEditProject(project)}>Edit</button>
             <button onClick={() => handleDeleteProject(project.id)}>Delete</button>
           </div>
-        ))}
-        <Link href="/">Home Page</Link>
-      </div>
-      {editingProject && (
-        <EditProjectModal 
-          project={editingProject} 
-          onSave={handleSaveProject} 
-          onCancel={() => setEditingProject(null)} 
-        />
-      )}
-    </main>
-  );
+);
+})}
+<Link href="/">Home Page</Link>
+</div>
+{editingProject && <EditProjectModal project={editingProject} save={handleSaveProject} />}
+</main>
+);
 };
 
 export default ProjectDetailsPage;
