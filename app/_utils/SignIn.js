2cc330b004624c4fabc7cf8ecbf4a06064ecd897
Sergei137 <sm1.618@protonmail.com>
2023-12-06 17:54:30 -0700

    updated firebase user info and auth context

--- app/_utils/SignIn.js
+++ app/_utils/SignIn.js
@@ -1,36 +1,34 @@
 'use client';
 import React, { useState } from 'react';
-
-
+import '../css/SignIn.css'
 
 import { auth } from './firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import '../css/SignIn.css'
 
-const SignIn = () => {
+const SignIn = ({ onLogInSuccess }) => {
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   
-  
-  
-  const signIn = (e) => {
-
+  const signIn = async (e) => {
+    
+    // 
     e.preventDefault();
 
-    setErrorMessage(''); // clear error message on new sign in
+    // clear error message on new sign in
+    setErrorMessage(''); 
 
-    signInWithEmailAndPassword(auth, email, password)
-
-    .then((userCredential) => {
+    // sign in user
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
       setErrorMessage(''); // clear error message on successful sign in
       console.log(userCredential);
-
-    }) 
-    .catch((error) => {
+    }
+    catch (error) {
       setErrorMessage('Failed to login. Incorrect email or password.'); // set error message on failed sign in
       console.log(error);
-    });
+    }
   };
 
   return (
@@ -57,16 +55,3 @@ const SignIn = () => {
 };
 
 export default SignIn;
-
-/*
-const signIn = async (e) => {
-  e.preventDefault();
-
-  try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    console.log(userCredential);
-  } catch (error) {
-    console.log(error);
-  }
-};
-*/
