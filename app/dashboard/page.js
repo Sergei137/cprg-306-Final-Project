03579e7e72e1682d5cd717f32ded5b0d8ac8d842
IuliaNeuc <Lulia.Neuc@edu.sait.ca>
2023-12-07 00:00:09 -0700

    Added some css

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -5,6 +5,7 @@ import NavBar from '@/components/NavBar';
 import ProjectDetails from '../projectDetails/page';
 
 import { useAuth } from '../_utils/AuthDetails';
+import './dashboard.css';
 
 export default function Dashboard() {
   // const projects = [
@@ -31,24 +32,48 @@ export default function Dashboard() {
     setProjects(savedProjects);
   }, []);
 
+  const colors = [
+    {
+      primaryColor : "#5D93E1",
+      secondaryColor : "#ECF3FC"
+    },
+    {
+      primaryColor : "#F9D288",
+      secondaryColor : "#FEFAF1"
+    },
+    {
+      primaryColor : "#5DC250",
+      secondaryColor : "#F2FAF1"
+    },
+    {
+      primaryColor : "#F48687",
+      secondaryColor : "#FDF1F1"
+    },
+    {
+      primaryColor : "#B964F7",
+      secondaryColor : "#F3F0FD"
+    }
+  ]
+
   return (
     <main>
       <NavBar/>
-      <div>
-        <p>Dashboard</p>
-        <p>-</p>
-        <Link href="/">Home/Log in page</Link>
+
+      <div className='proj-content-container'>
+          <div className='proj-info-container'>
+          {projects.map((project, index) => (
+            <div key={index} className='proj-container'>
+              <h2 style={{"background-color": colors[index%5].primaryColor}}>{project.Name}</h2>
+              <p style={{"background-color": colors[index%5].secondaryColor}}>{project.Description}</p>
+              <p className='font-medium'>Due date:</p><p> {project.Duedate}</p>
+              <p className='font-medium'>Team members: </p><p>{project.Teammember.join(', ')}</p>
+              <p className='font-medium'>Progress:</p><p> {project.Progress}%</p>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className='content-container'>
-        {projects.map((project, index) => (
-          <div key={index}>
-            <h2>{project.Name}</h2>
-            <p>{project.Description}</p>
-            <p>Due date: {project.Duedate}</p>
-            <p>Team members: {project.Teammember.join(', ')}</p>
-            <p>Progress: {project.Progress}%</p>
-          </div>
-        ))}
+      <div>
+        <Link href="/">Home/Log in page</Link>
       </div>
     </main>
   )
